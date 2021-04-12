dea1326cc21a35b12048893c78ed3eb1b8ec0b32
Kei5665 <kt10195665@yahoo.ne.jp>
2021-04-12 13:34:25 +0000

    ちょっとした仕様変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @posts = Post.includes(:user).page(params[:page])
+    @posts = Post.includes(:user).page(params[:page]).order(created_at: :desc)
   end
 
   def new
